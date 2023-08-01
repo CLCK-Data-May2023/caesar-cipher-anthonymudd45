02b4de473739c3f0c6811fb91cf9c3d541edee2d
Tony Mudd <134401350+anthonymudd45@users.noreply.github.com>
2023-08-01 15:10:35 -0400

    Update cipher.py

--- cipher.py
+++ cipher.py
@@ -1,1 +1,13 @@
 # add your code here
+alphabet = "abcdefghijklmnopqrstuvwxyz"
+cypher = "fghijklmnopqrstuvwxyzabcde"
+encryption = {}
+i = 0
+for letter in alphabet:
+    encryption[letter] = cypher[i]
+    i += 1
+
+message = str(input("Please enter a sentence:")).lower()
+
+output = [encryption.get(letter, letter) for letter in message]
+print("".join(output))
