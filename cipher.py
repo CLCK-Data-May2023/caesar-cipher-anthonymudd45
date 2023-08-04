1354dbab1b95a37521d83a7762a289f84be1ee82
Tony Mudd <134401350+anthonymudd45@users.noreply.github.com>
2023-08-04 12:42:23 -0400

    cipher

--- cipher.py
+++ cipher.py
@@ -7,7 +7,7 @@ for letter in alphabet:
     encryption[letter] = cypher[i]
     i += 1
 
-message = str(input("Please enter a sentence:")).lower()
+message = str(input("Please enter a message:")).lower()
 
 output = [encryption.get(letter, letter) for letter in message]
 print("".join(output))
