e0dcde27fdfde53c632bc2027a52fa5b087a87a9
Tony Mudd <134401350+anthonymudd45@users.noreply.github.com>
2023-08-05 13:01:36 -0400

    cipher

--- cipher.py
+++ cipher.py
@@ -1,12 +1,10 @@
 # add your code here
-alphabet = {
-    'a':'f', 'b':'g', 'c':'h', 'd':'i', 'e':'j', 'f':'k', 'g':'l', 'h':'m', 'i':'n', 'j':'o',
-    'k':'p', 'l':'q', 'm':'r', 'n':'s', 'o':'t', 'p':'u', 'q':'v', 'r':'w', 's':'x', 't':'y',
-    'u':'z', 'v':'a', 'w':'b', 'x':'c', 'y':'d', 'z':'e'
-    } 
 message = input('Please enter a sentence:')
 message = message.lower()
 
+alphabet = {
+    'a':'f', 'b':'g', 'c':'h', 'd':'i', 'e':'j', 'f':'k', 'g':'l', 'h':'m', 'i':'n', 'j':'o',
+    'k':'p', 'l':'q', 'm':'r', 'n':'s', 'o':'t', 'p':'u', 'q':'v', 'r':'w', 's':'x', 't':'y','u':'z', 'v':'a', 'w':'b', 'x':'c', 'y':'d', 'z':'e'}
 cypher = ''
 for letter in message:
 
